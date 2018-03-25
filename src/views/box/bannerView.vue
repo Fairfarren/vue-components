15d6441524a436fd8ad8c076eb3991597f3c8ca7
Fairfarren <lykwowlol@gmail.com>
2018-03-25 20:09:27 +0800

    添加banner的跳转功能

--- src/views/box/bannerView.vue
+++ src/views/box/bannerView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <fairfarrenBanner :dataList="dataList" timeOut="1000"/>
+    <fairfarrenBanner :dataList="dataList" timeOut="000"/>
   </div>
 </template>
 
@@ -9,17 +9,23 @@ export default {
   data () {
     return {
       dataList: [{
-        img: 'https://i.loli.net/2018/03/25/5ab75a03c5ee2.jpeg'
+        img: 'https://i.loli.net/2018/03/25/5ab75a03c5ee2.jpeg',
+        path: 'https://www.baidu.com'
       }, {
-        img: 'https://i.loli.net/2018/03/25/5ab75a0538920.jpeg'
+        img: 'https://i.loli.net/2018/03/25/5ab75a0538920.jpeg',
+        path: '/card'
       }, {
-        img: 'https://i.loli.net/2018/03/25/5ab75a0f40956.jpeg'
+        img: 'https://i.loli.net/2018/03/25/5ab75a0f40956.jpeg',
+        path: '/hello'
       }, {
-        img: 'https://i.loli.net/2018/03/25/5ab75a130047b.jpg'
+        img: 'https://i.loli.net/2018/03/25/5ab75a130047b.jpg',
+        path: '/home'
       }, {
-        img: 'https://i.loli.net/2018/03/25/5ab75a1351249.jpeg'
+        img: 'https://i.loli.net/2018/03/25/5ab75a1351249.jpeg',
+        path: 'http://www.google.com'
       }, {
-        img: 'https://i.loli.net/2018/03/25/5ab75a13f22d6.jpg'
+        img: 'https://i.loli.net/2018/03/25/5ab75a13f22d6.jpg',
+        path: 'asdfasdf'
       }]
     }
   }
