bd7f4d41011d428ea16ddd45c98a9263aabab94c
Fairfarren <lykwowlol@gmail.com>
2018-04-08 10:46:27 +0800

    multipleSelect，添加正则验证方法

--- src/views/box/multipleSelect/multipleSelectView.vue
+++ src/views/box/multipleSelect/multipleSelectView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <fairfarrenMultipleSelect theKey="input01" :list="list"/>
+    <fairfarrenMultipleSelect theKey="input01" :list="list" :reg="mobile"/>
   </div>
 </template>
 
@@ -8,7 +8,11 @@
 export default {
   data () {
     return {
-      list: [1]
+      list: [1],
+      mobile: {
+        reg: /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/,
+        text: '手机号不正确'
+      }
     }
   }
 }
